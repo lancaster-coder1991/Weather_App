dd758e546f3a5829aa9cdf9ae787975d817f585c
lancaster-coder1991 <grjscott1991@hotmail.com>
2020-10-02 17:06:08 +0100

    add CSS

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,13 @@
 const goBtn = document.getElementById("go");
+const errorMsg = document.getElementById("errorMsg");
 
 returnWeather = (city) => {
   return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=London&appid=8bbfb4c5fe492461d4fe061e82d3dff6&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city},,GB&appid=8bbfb4c5fe492461d4fe061e82d3dff6&units=metric`
   )
     .then((response) => {
-      if (response.status !== 200) {
-        console.log(
-          "Looks like there was a problem. Status Code: " + response.status
-        );
+      if (response.status !== 200 || city === "") {
+        throw 404;
       } else {
         return response.json();
       }
@@ -17,73 +16,77 @@ returnWeather = (city) => {
       return data;
     })
     .catch((err) => {
-      console.log(err);
+      return err;
     });
 };
 
 goBtn.addEventListener("click", () => {
   const city = document.getElementById("city").value;
-  console.log(city);
   returnWeather(city).then((data) => {
-    console.log(data);
-
-    //Create array of result fields
-    const resultSpans = Array.from(
-      document.getElementsByClassName("resultSpan")
-    );
-
-    //Reformat the description to be Title Case
-    let description = (
-      data.weather[0].description[0].toUpperCase() +
-      data.weather[0].description.slice(1)
-    ).replace(/\s[a-z]/g, (match) => {
-      return match.toUpperCase();
-    });
-
-    //Fill in Description field
-    resultSpans.find(
-      (span) => span.id === "Description"
-    ).innerHTML = `Description: ${description}`;
-
-    //Fill in Temperature field
-    resultSpans.find(
-      (span) => span.id === "Temperature"
-    ).innerHTML = `Temperature: ${data.main.temp}${String.fromCharCode(176)}C`;
-
-    //Fill in Feels Like field
-    resultSpans.find(
-      (span) => span.id === "FeelsLike"
-    ).innerHTML = `Feels Like: ${data.main.feels_like}${String.fromCharCode(
-      176
-    )}C`;
-
-    //Set cloudiness description
-    const cloudPercentage = data.clouds.all;
-    let cloudDesc;
-    if (cloudPercentage < 26) {
-      cloudDesc = "Not Very Cloudy";
-    } else if (cloudPercentage < 51) {
-      cloudDesc = "A Little Cloudy";
-    } else if (cloudPercentage < 76) {
-      cloudDesc = "Pretty Cloudy";
+    if (data === 404) {
+      errorMsg.innerHTML = "Error - City not found";
     } else {
-      cloudDesc = "Very Cloudy";
+      errorMsg.innerHTML = "";
+      //Create array of result fields
+      const resultSpans = Array.from(
+        document.getElementsByClassName("resultSpan")
+      );
+
+      //Reformat the description to be Title Case
+      let description = (
+        data.weather[0].description[0].toUpperCase() +
+        data.weather[0].description.slice(1)
+      ).replace(/\s[a-z]/g, (match) => {
+        return match.toUpperCase();
+      });
+
+      //Fill in Description field
+      resultSpans.find(
+        (span) => span.id === "Description"
+      ).innerHTML = `Description: ${description}`;
+
+      //Fill in Temperature field
+      resultSpans.find(
+        (span) => span.id === "Temperature"
+      ).innerHTML = `Temperature: ${data.main.temp}${String.fromCharCode(
+        176
+      )}C`;
+
+      //Fill in Feels Like field
+      resultSpans.find(
+        (span) => span.id === "FeelsLike"
+      ).innerHTML = `Feels Like: ${data.main.feels_like}${String.fromCharCode(
+        176
+      )}C`;
+
+      //Set cloudiness description
+      const cloudPercentage = data.clouds.all;
+      let cloudDesc;
+      if (cloudPercentage < 26) {
+        cloudDesc = "Not Very Cloudy";
+      } else if (cloudPercentage < 51) {
+        cloudDesc = "A Little Cloudy";
+      } else if (cloudPercentage < 76) {
+        cloudDesc = "Pretty Cloudy";
+      } else {
+        cloudDesc = "Very Cloudy";
+      }
+
+      //Fill in Cloudiness field
+      resultSpans.find(
+        (span) => span.id === "Cloudiness"
+      ).innerHTML = `Cloudiness: ${cloudPercentage}% - ${cloudDesc}`;
+
+      //Fill in Cloudiness field
+      resultSpans.find(
+        (span) => span.id === "WindSpeed"
+      ).innerHTML = `Wind Speed: ${data.wind.speed}m/s`;
+
+      //Fill in Humidity field
+      resultSpans.find(
+        (span) => span.id === "Humidity"
+      ).innerHTML = `Humidity: ${data.main.humidity}%`;
     }
-
-    //Fill in Cloudiness field
-    resultSpans.find(
-      (span) => span.id === "Cloudiness"
-    ).innerHTML = `Cloudiness: ${cloudPercentage}% - ${cloudDesc}`;
-
-    //Fill in Cloudiness field
-    resultSpans.find(
-      (span) => span.id === "WindSpeed"
-    ).innerHTML = `Wind Speed: ${data.wind.speed}m/s`;
-
-    //Fill in Humidity field
-    resultSpans.find(
-      (span) => span.id === "Humidity"
-    ).innerHTML = `Humidity: ${data.main.humidity}%`;
   });
 });
 
