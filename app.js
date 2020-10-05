372dac2ec900072f34423dd78136a8d145cfdcac
lancaster-coder1991 <grjscott1991@hotmail.com>
2020-10-05 16:20:19 +0100

    add moving weather background

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
-const apiRouter = require("./Routers/api.router.js");
-const { returnWeather } = require("./js/functions");
+// const express = require("express");
+// const app = express();
+// const apiRouter = require("./Routers/api.router.js");
+// const { returnWeather } = require("./js/functions");
 
-app.use(express.json());
-app.get("/api", returnWeather);
+// app.use(express.json());
+// app.get("/api", returnWeather);
 
-app.listen(9090, () => {
-  console.log("this server is running on port 9090");
-});
+// app.listen(9090, () => {
+//   console.log("this server is running on port 9090");
+// });
